9f4aeff55a4c955192ccb63de1c448287a76699c
Iqra Fatima <141397977+IqraFatima-Coder@users.noreply.github.com>
2025-01-21 00:43:57 +0500

    Delete DSA_Lab/DSA_Lab_Tasks_CodeFiles/DSA_L1/Task8.py
    Deleted Duplicate Task8 File

--- DSA_Lab/DSA_Lab_Tasks_CodeFiles/DSA_L1/Task8.py
+++
@@ -1,8 +0,0 @@
-#Author: Iqra Fatima
-#Reg. Number: 23-CP-62
-#Task 8: Python Tuples
-colors = ("red","green","blue")
-print("First color:",colors[0])
-#Unpacking tuple
-(a,b,c)=colors
-print("Unpacked values",a,b,c)
