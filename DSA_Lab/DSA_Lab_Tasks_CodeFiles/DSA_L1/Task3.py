191828bfb812f5f3287dd91d67885ffc84f2ffe0
Iqra Fatima <iqrafatima1717@gmail.com>
2025-01-20 06:01:55 +0500

    Added Task3

---
+++ DSA_Lab/DSA_Lab_Tasks_CodeFiles/DSA_L1/Task3.py
@@ -0,0 +1,12 @@
+#Author: Iqra Fatima
+#Reg. Number: 23-CP-62
+#Task 3: Python Numbers and Type Conversions
+num=10
+decimal=5.7
+text="123"
+#Arithmentic Operations
+print("Sum:",num+decimal)
+print("Product:",num*decimal)
+#Type Conversions
+print("Integer to Float:",float(num))
+print("String to Integer:",int(text))
