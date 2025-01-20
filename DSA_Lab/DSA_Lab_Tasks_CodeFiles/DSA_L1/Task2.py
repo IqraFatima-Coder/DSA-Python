47f8a5db68b3f55bbb97ae0189fd94c1db152c35
Iqra Fatima <iqrafatima1717@gmail.com>
2025-01-20 05:49:11 +0500

    Added Task2

---
+++ DSA_Lab/DSA_Lab_Tasks_CodeFiles/DSA_L1/Task2.py
@@ -0,0 +1,13 @@
+#Author: Iqra Fatima
+#Reg. Number: 23-CP-62
+#Task 2: Python Data Types
+#Data Types
+integer_value=10
+float_value=10.5
+string_value="Hello Python"
+boolean_value=True
+#Printing Data Types
+print(type(integer_value))
+print(type(float_value))
+print(type(string_value))
+print(type(boolean_value))
