4048569c03415e009b4388780d3c929e3a4e07bf
Iqra Fatima <iqrafatima1717@gmail.com>
2025-01-20 05:35:52 +0500

    Added_Task1

---
+++ DSA_Lab/DSA_Lab_Tasks_CodeFiles/DSA_L1/Task1.py
@@ -0,0 +1,12 @@
+#Author: Iqra Fatima
+#Reg. Number: 23-CP-62
+#Task 1: Python Variables
+#Defining Variables
+name="Alice"
+age=25
+is_student=True
+#Printing Variables
+print("Name:",name)
+print("Age:",age)
+print("Is Student:",is_student)
+
